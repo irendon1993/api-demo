f2042348c4f8112a7ef4bc8e31eb02d5f3f353e4
irendon1993 <isaacrendon7@gmail.com>
2022-05-03 14:57:42 -0400

    Edited routes and created product model

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'products/index'
-      get 'products/show'
+      resources :products, only: [:index, :show, :create]
     end
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
